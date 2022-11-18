322d9b68b7e6bc8951a8d0dd8f2c29399ac0a586
osakkiii <117650697+osakkiii@users.noreply.github.com>
2022-11-18 13:46:23 +0000

    [Start]3-5

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#creat'
   get 'lists' => 'lists#index'
   get 'lists/:id' => 'lists#show', as: 'list'
-  get 'lists/edit'
+  get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
